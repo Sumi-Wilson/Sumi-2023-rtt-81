b19a3d433654998dc1093bbc8f78985bd045e87c
Sumi-Wilson <sumikokkadan@gmail.com>
2023-09-18 15:38:41 -0500

    string operations

---
+++ Stringoperations/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module stroperation {
+}
---
+++ Stringoperations/src/stroperation/StrOperation.java
@@ -0,0 +1,10 @@
+package stroperation;
+
+public class StrOperation {
+	public static void main(String[]args) {
+		String name = "pEr SchOlas";
+		System.out.println(name.toUpperCase());
+	}
+	
+
+}
