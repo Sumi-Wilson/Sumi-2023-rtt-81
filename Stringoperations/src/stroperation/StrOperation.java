e6cac8907066ec654f43913c5ec0d6aa891eb2f8
Sumi-Wilson <sumikokkadan@gmail.com>
2023-09-18 15:46:37 -0500

    toLowercase

--- Stringoperations/src/stroperation/StrOperation.java
+++ Stringoperations/src/stroperation/StrOperation.java
@@ -4,6 +4,9 @@ public class StrOperation {
 	public static void main(String[]args) {
 		String name = "pEr SchOlas";
 		System.out.println(name.toUpperCase());
+		String s2 = name.toLowerCase();
+		System.out.println(s2);
+		
 	}
 	
 
