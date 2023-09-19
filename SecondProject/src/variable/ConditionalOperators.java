a4961cbe6a63adc1fe5064514601151ce53169d7
Sumi-Wilson <sumikokkadan@gmail.com>
2023-09-19 14:28:27 -0500

    Conditional Operators

---
+++ SecondProject/src/variable/ConditionalOperators.java
@@ -0,0 +1,32 @@
+package variable;
+
+public class ConditionalOperators {
+	public static void main(String[]args) {
+		boolean cond1 = true;
+		boolean cond2 =false;
+		if (cond1 && cond2) {
+			System.out.println("both variables are true");
+		}
+		if (cond1 || cond2) {
+			System.out.println("either or both the variable is true");
+			}
+		if (!cond1 && !cond2) {
+			System.out.println("niether is true");
+		}
+		int x=10;
+		if((x>3 ) && (x<20)) {
+			System.out.println("x is between 3 & 20");
+		}
+		int y=4;
+		if(y%2==1) {
+			System.out.println("this a odd number");
+		}
+	     else {
+				System.out.println("This is a even number ");
+			}
+		
+		}
+		}
+	
+
+
