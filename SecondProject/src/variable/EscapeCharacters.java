5fc175524e35151da2d8e943cdd5f3a1c78b23bc
Sumi-Wilson <sumikokkadan@gmail.com>
2023-09-19 13:21:22 -0500

    Input from keyboard

--- SecondProject/src/variable/EscapeCharacters.java
+++ SecondProject/src/variable/EscapeCharacters.java
@@ -2,11 +2,14 @@ package variable;
 
 public class EscapeCharacters {
 	public static void main(String[]args) {
-		System.out.println("this is a tab character-\t -");
+		System.out.println("this is a tab character->\t< -");
+		//new line character
 		System.out.println("this is first line\nthis is secomd line");
+		//basic formating with tabs
 		System.out.println("This is a back slash\\");
 		System.out.println("1\tone\t\tcol3");
 		System.out.println("10\tten\t\tcol3");
+		//how to put \
 		System.out.println("100\thundred\t\tcol3");
 	}
 
