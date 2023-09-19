4668d8a22af533eeee3b851e5bc1e049845ac196
Sumi-Wilson <sumikokkadan@gmail.com>
2023-09-19 15:48:35 -0500

    GLAB LogicalOperatorsDemo

---
+++ SecondProject/src/variable/LogicalOperatorsDemo.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class LogicalOperatorsDemo {
+	public static void main(String[]args) {
+		   boolean x = false;
+	       boolean y = false;
+	       System.out.println("x & y : " + (x & y));
+	       System.out.println("x && y : " + (x && y));
+	       System.out.println("x | y : " + (x | y));
+	       System.out.println("x || y: " + (x || y));
+	       System.out.println("x ^ y : " + (x ^ y));
+	       System.out.println("!x : " + (!x));
+
+	}
+
+}
