928f70c6acfb1b53542287cb5cd983f960a395d5
Sumi-Wilson <sumikokkadan@gmail.com>
2023-09-25 19:46:00 -0500

    Controlling a Loop with a Sentinel Value

---
+++ SecondProject/src/loops/SentinalValueDemo.java
@@ -0,0 +1,23 @@
+package loops;
+
+//Example #3: Controlling a Loop with a Sentinel Value
+import java.util.Scanner;
+
+public class SentinalValueDemo {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an int value (the program exits if the input is 0): ");
+		int data = input.nextInt();
+		int sum = 0;
+		while (data != 0) {
+			sum += data;
+			System.out.print("Enter an int value (the program exits if the input is 0): ");
+			data = input.nextInt();
+
+		}
+
+		System.out.println("The sum is " + sum);
+
+	}
+
+}
