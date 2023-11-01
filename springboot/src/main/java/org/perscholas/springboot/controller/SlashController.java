30e9ddfd4aa3dc12a0882780cc80f6d169501131
Sumi-Wilson <sumikokkadan@gmail.com>
2023-11-01 15:21:44 -0500

    Spring boot User class and SlashController class

---
+++ springboot/src/main/java/org/perscholas/springboot/controller/SlashController.java
@@ -0,0 +1,14 @@
+package org.perscholas.springboot.controller;
+
+
+import org.perscholas.springboot.database.entity.User;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SlashController {
+    public void index() {
+        User user = new User();
+        user.setId(1);
+        user.setEmail("sumigeorgek@hotmail.com");
+    }
+}
