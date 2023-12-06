29eed30c864df02aa731d0e85b694f100f1662d0
Sumi-Wilson <sumikokkadan@gmail.com>
2023-12-06 14:19:40 -0600

    resigistering user with password encrypted on dec 6

--- springboot/src/main/java/org/perscholas/springboot/service/UserService.java
+++ springboot/src/main/java/org/perscholas/springboot/service/UserService.java
@@ -1,21 +1,34 @@
 package org.perscholas.springboot.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.util.stereotypes.Lazy;
 import org.perscholas.springboot.database.dao.UserDAO;
 import org.perscholas.springboot.database.entity.User;
 import org.perscholas.springboot.formbean.RegisterUserFormBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Slf4j
 @Service
 public class UserService {
     @Autowired
     private UserDAO userDao;
+
+    @Lazy
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public User createUser(RegisterUserFormBean form){
         User user= new User();
         user.setEmail(form.getEmail());//.toLowerCase
-        user.setPassword(form.getPassword());
+
+        String encoded = passwordEncoder.encode(form.getPassword());
+        log.debug("Encoded password: " + encoded);
+        user.setPassword(encoded);
+        
+
         return userDao.save(user);
 
     }
